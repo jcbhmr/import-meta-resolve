b0e9238b00684cb631d091ef83a4bb0dba67c111
Jacob Hummer <jcbhmr@outlook.com>
2023-07-05 02:11:53 -0500

    add test for loaders

--- index.test.js
+++ index.test.js
@@ -1,8 +1,19 @@
 import test from "node:test";
 import assert from "node:assert";
+import os from "node:os";
+import fs from "node:fs";
+import fsPromises from "node:fs/promises";
+import { $ } from "execa";
 import applyResolvePolyfill from "./index.js";
 
 const importMeta = applyResolvePolyfill({ url: import.meta.url });
+
+async function tmpjs(js) {
+  const f = os.tmpdir() + "/" + Math.random().toString(36) + ".mjs";
+  await fsPromises.writeFile(f, js);
+  process.on("exit", () => fs.unlinkSync(f));
+  return f;
+}
 
 test("resolve works with relative paths", () => {
   console.log(importMeta.resolve("./index.js"));
@@ -33,4 +44,22 @@ test("resolve works with node: specifiers", () => {
 test("works with no URL", () => {
   const importMeta = applyResolvePolyfill();
   console.log(importMeta.resolve("./index.js"));
+});
+
+test("works with loaders", async () => {
+  const f1 = await tmpjs(`
+    export function resolve(specifier, ctx, next) {
+      if (specifier.startsWith("custom:")) {
+        return { url: "hello:world", shortCircuit: true };
+      }
+      return next(specifier, ctx);
+    }
+  `);
+  const u = importMeta.resolve("./index.js");
+  const f2 = await tmpjs(`
+    import applyResolvePolyfill from ${JSON.stringify(u)};
+    applyResolvePolyfill(import.meta);
+    console.log(import.meta.resolve("custom:foo"));
+  `);
+  console.log((await $`node --loader=${f1} ${f2}`).stdout);
 });
